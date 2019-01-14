7d8271b8c2e6b901d2cc6c4dca1bcbeeb18a741a
Anthony DiPadova <tony.19@dartmouth.edu>
2019-01-14 18:57:53 -0500

    Added map view and marker state

--- maps/App.js
+++ maps/App.js
@@ -26,6 +26,55 @@ const CARD_HEIGHT = height / 4;
 const CARD_WIDTH = CARD_HEIGHT - 50;
 
 export default class screens extends Component {
+
+// define our marker state
+state = {
+    markers: [
+      {
+        coordinate: {
+          latitude: 45.524548,
+          longitude: -122.6749817,
+        },
+        title: "Best Place",
+        description: "This is the best place in Portland",
+        image: Images[0],
+      },
+      {
+        coordinate: {
+          latitude: 45.524698,
+          longitude: -122.6655507,
+        },
+        title: "Second Best Place",
+        description: "This is the second best place in Portland",
+        image: Images[1],
+      },
+      {
+        coordinate: {
+          latitude: 45.5230786,
+          longitude: -122.6701034,
+        },
+        title: "Third Best Place",
+        description: "This is the third best place in Portland",
+        image: Images[2],
+      },
+      {
+        coordinate: {
+          latitude: 45.521016,
+          longitude: -122.6561917,
+        },
+        title: "Fourth Best Place",
+        description: "This is the fourth best place in Portland",
+        image: Images[3],
+      },
+    ],
+    region: {
+      latitude: 45.52220671242907,
+      longitude: -122.6653281029795,
+      latitudeDelta: 0.04864195044303443,
+      longitudeDelta: 0.040142817690068,
+    },
+  };
+
   // runs when app loads
   componentWillMount() {
     this.index = 0;
@@ -35,7 +84,13 @@ export default class screens extends Component {
   render() {
     return (
       <View style={styles.container}>
-        
+        <MapView
+  ref={map => this.map = map}
+  initialRegion={this.state.region}
+  style={styles.container}
+>
+
+</MapView>
       </View>
     );
   }
